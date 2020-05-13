2f23e71749187758fc552bfb315d76790571ae42
Patrick Tsai <patosai@users.noreply.github.com>
2020-05-12 20:01:54 -0700

    Get a better image for plotting epipolar lines

--- slam.py
+++ slam.py
@@ -47,7 +47,7 @@ matches = bf.match(img1_des, img2_des)
 # - DMatch.queryIdx - Index of the descriptor in query descriptors
 # - DMatch.imgIdx - Index of the train image.
 matches = sorted(matches, key = (lambda x: x.distance))
-selected_matches = [match for match in matches if match.distance < 48]
+selected_matches = [match for match in matches if match.distance < 32]
 
 if False:
     matches_to_draw = selected_matches
@@ -56,14 +56,16 @@ if False:
     plt.show()
 
 # opencv images store y coord first, then x coord
-matched_points = [[[img1_kp[match.trainIdx].pt[1], img1_kp[match.trainIdx].pt[0]],
-                   [img2_kp[match.queryIdx].pt[1], img2_kp[match.queryIdx].pt[0]]]
-                  for match in selected_matches]
+matched_points = np.asarray([[[img1_kp[match.trainIdx].pt[1], img1_kp[match.trainIdx].pt[0]],
+                              [img2_kp[match.queryIdx].pt[1], img2_kp[match.queryIdx].pt[0]]]
+                             for match in selected_matches])
 fundamental_matrix = eight_point_algorithm.calculate_fundamental_matrix(matched_points)
 
 if False:
     matches_to_draw = selected_matches[:8]
-    line_equations = [fundamental_matrix @ np.asarray([img1_kp[match.trainIdx].pt[1], img1_kp[match.trainIdx].pt[0], 1])
+
+    # draw epipolar lines on left image
+    line_equations = [np.asarray([img2_kp[match.queryIdx].pt[1], img2_kp[match.queryIdx].pt[0], 1]) @ fundamental_matrix
                       for match in matches_to_draw]
     left_img = img1.copy()
     for a, b, c in line_equations:
@@ -75,13 +77,16 @@ if False:
                  (255, 0, 0),
                  thickness=2)
 
+    # draw epipolar lines on the right image
+    line_equations = [fundamental_matrix @ np.asarray([img1_kp[match.trainIdx].pt[1], img1_kp[match.trainIdx].pt[0], 1]).transpose()
+                      for match in matches_to_draw]
     right_img = img2.copy()
-    for match in matches_to_draw:
-        cv2.circle(right_img,
-                   (int(img2_kp[match.queryIdx].pt[0]), int(img2_kp[match.queryIdx].pt[1])),
-                   15,
-                   (255, 0, 0),
-                   thickness=4)
+    for a, b, c in line_equations:
+        cv2.line(right_img,
+                 (int(-c / b), 0),
+                 (int(-(c + a * right_img.shape[1]) / b), right_img.shape[1]),
+                 (255, 0, 0),
+                 thickness=2)
 
     fig = plt.figure()
     fig.add_subplot(1, 2, 1)
@@ -89,9 +94,8 @@ if False:
 
     fig.add_subplot(1, 2, 2)
     plt.imshow(right_img)
+
     plt.show()
 
 essential_matrix = np.transpose(intrinsic_camera_matrix) @ fundamental_matrix @ intrinsic_camera_matrix
-rotation, translation = essential.essential_matrix_to_rotation_translation(essential_matrix)
-print(rotation)
-print(translation)
+rotation, translation = essential.essential_matrix_to_rotation_translation(essential_matrix, matched_points)
--- src/essential.py
+++ src/essential.py
@@ -1,15 +1,25 @@
 import numpy as np
 
 
-def essential_matrix_to_rotation_translation(e):
+def essential_matrix_to_rotation_translation(essential_matrix, matched_points):
     "given an essential matrix, calculates the translation vector (normalized) and the rotation matrix"
-    u, s, v = np.linalg.svd(e)
+    u, s, v = np.linalg.svd(essential_matrix)
     # http://igt.ip.uca.fr/~ab/Classes/DIKU-3DCV2/Handouts/Lecture16.pdf
     w = np.asarray([[0, -1, 0],
                     [1, 0, 0],
                     [0, 0, 1]])
-    rotation = u @ w @ v
+    possible_rotations = [u @ w @ v, u @ np.transpose(w) @ v]
+    rotation_multiplier = 1 if np.linalg.det(possible_rotations[0]) > 0 else -1
+    possible_rotations = [rotation_multiplier * rotation for rotation in possible_rotations]
+
     # translation X points right, Y points down, Z points into the image
-    translation = u[:, 2]
+    possible_translations = [u[:, -1], -1*u[:, -1]]
+
+    for rotation in possible_rotations:
+        for translation in possible_translations:
+            point_a = matched_points[0][0]
+            #print([point_a; 1] - translation)
+
+
     # TODO need to test points to determine the correct solution out of the four
     return rotation, translation
